dd253dcc8aa90dfa2fb6a77713e49c02cd2590f5
judiemosinabre <mosinabrejp@students.nu-dasma.edu.ph>
2026-05-17 10:24:56 +0800

    chore: reupload

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -131,4 +131,5 @@ export default function Hero() {
       <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-black to-transparent pointer-events-none z-20" />
     </section>
   );
+  
 }
